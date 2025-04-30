17aa6e5d9f9c1285e5c6b10e42117f5253934844
Sergey <s.leonov@astrovolga.ru>
2025-04-30 09:35:46 +0300

    First commit

---
+++ components/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <button type="button" @click="showBlock = !showBlock">
+    Toggle block
+  </button>
+  <Block v-if="showBlock" />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Block from './Block.vue';
+
+const showBlock = ref(false);
+</script>
---
+++ components/Block.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>Block with internal reactive data</div>
+</template>
+
+<script setup>
+import { onBeforeMount, ref } from 'vue';
+
+const data = ref({});
+
+onBeforeMount(loadData);
+
+async function loadData() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+
+    data.value = await response.json();
+
+    console.log(11111, data.value);
+  } catch (e) {
+    console.log(e);
+  }
+}
+</script>
